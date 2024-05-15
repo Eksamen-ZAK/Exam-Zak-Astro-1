4d257b00af1dc132e5187a34c27df70563eec013
katbang <katr164m@stud.kea.dk>
2024-05-15 19:31:48 +0200

    stat.js

---
+++ src/js/stats.js
@@ -0,0 +1,25 @@
+let uuid;
+
+if (localStorage.getItem("uuid")) {
+  uuid = localStorage.getItem("uuid");
+} else {
+  uuid = sessionStorage.getItem("uuid");
+}
+
+const url = `https://jlgsxiynwqvvhwheexwo.supabase.co/rest/v1/user-data?id=eq.${uuid}`;
+const api =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpsZ3N4aXlud3F2dmh3aGVleHdvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQ0NzA5MjksImV4cCI6MjAzMDA0NjkyOX0.U40ZZWRh_MC7612vdwFHVKFZxwRHq_TECCnnzovEXKE";
+
+const options = {
+  method: "GET",
+  headers: {
+    apikey: api,
+  },
+};
+
+const data = await fetch(url, options).then((res) => {
+  if (!res.ok) {
+    window.location.href = "/";
+  }
+  return res.json();
+});
