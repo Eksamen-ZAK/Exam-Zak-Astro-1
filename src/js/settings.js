2c1c1ace4674a15ab2779699e9406cc979df2930
zasia meincke <zasi0001@kea.dk>
2024-05-19 12:49:12 +0200

    #312 text-to-speech button on/of

--- src/js/settings.js
+++ src/js/settings.js
@@ -9,8 +9,7 @@ if (localStorage.getItem("uuid")) {
 }
 
 const url = `https://jlgsxiynwqvvhwheexwo.supabase.co/rest/v1/user-data?id=eq.${uuid}`;
-const api =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpsZ3N4aXlud3F2dmh3aGVleHdvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQ0NzA5MjksImV4cCI6MjAzMDA0NjkyOX0.U40ZZWRh_MC7612vdwFHVKFZxwRHq_TECCnnzovEXKE";
+const api = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpsZ3N4aXlud3F2dmh3aGVleHdvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQ0NzA5MjksImV4cCI6MjAzMDA0NjkyOX0.U40ZZWRh_MC7612vdwFHVKFZxwRHq_TECCnnzovEXKE";
 
 const options = {
   method: "GET",
@@ -29,59 +28,47 @@ const data = await fetch(url, options).then((res) => {
 });
 const html = document.querySelector("html");
 
-document
-  .querySelector(".text-size-setting")
-  .addEventListener("mousedown", () => {
-    document.getElementById("text-setting-modal").showModal();
-  });
+document.querySelector(".text-size-setting").addEventListener("mousedown", () => {
+  document.getElementById("text-setting-modal").showModal();
+});
 
-document
-  .querySelector(".text-close-button")
-  .addEventListener("mousedown", () => {
-    document.getElementById("text-setting-modal").close();
-    location.reload();
-  });
+document.querySelector(".text-close-button").addEventListener("mousedown", () => {
+  document.getElementById("text-setting-modal").close();
+  location.reload();
+});
 
-document
-  .querySelector(".contrast-setting")
-  .addEventListener("mousedown", () => {
-    document.getElementById("contrast-setting-modal").showModal();
-  });
+document.querySelector(".contrast-setting").addEventListener("mousedown", () => {
+  document.getElementById("contrast-setting-modal").showModal();
+});
 
-document
-  .querySelector(".contrast-close-button")
-  .addEventListener("mousedown", () => {
-    document.getElementById("contrast-setting-modal").close();
-    location.reload();
-  });
+document.querySelector(".contrast-close-button").addEventListener("mousedown", () => {
+  document.getElementById("contrast-setting-modal").close();
+  location.reload();
+});
 
-document
-  .getElementById("text-size-slider")
-  .addEventListener("input", function () {
-    html.style.setProperty("--fs-m", `${this.value}px`);
-    localStorage.setItem("font-size", this.value);
-  });
+document.getElementById("text-size-slider").addEventListener("input", function () {
+  html.style.setProperty("--fs-m", `${this.value}px`);
+  localStorage.setItem("font-size", this.value);
+});
 
-document
-  .getElementById("contrast-slider")
-  .addEventListener("input", function () {
-    console.log(this.value);
-    if (this.value === "18") {
-      html.style.setProperty("--primary-color", `#901a36`);
-      html.style.setProperty("--secondary-color", `#749e2e`);
-      localStorage.setItem("contrast", "low");
-    }
-    if (this.value === "24") {
-      html.style.setProperty("--primary-color", `#7A061F`);
-      html.style.setProperty("--secondary-color", `#597B20`);
-      localStorage.setItem("contrast", "medium");
-    }
-    if (this.value === "30") {
-      html.style.setProperty("--primary-color", `#6B061C`);
-      html.style.setProperty("--secondary-color", `#0F5400`);
-      localStorage.setItem("contrast", "high");
-    }
-  });
+document.getElementById("contrast-slider").addEventListener("input", function () {
+  console.log(this.value);
+  if (this.value === "18") {
+    html.style.setProperty("--primary-color", `#901a36`);
+    html.style.setProperty("--secondary-color", `#749e2e`);
+    localStorage.setItem("contrast", "low");
+  }
+  if (this.value === "24") {
+    html.style.setProperty("--primary-color", `#7A061F`);
+    html.style.setProperty("--secondary-color", `#597B20`);
+    localStorage.setItem("contrast", "medium");
+  }
+  if (this.value === "30") {
+    html.style.setProperty("--primary-color", `#6B061C`);
+    html.style.setProperty("--secondary-color", `#0F5400`);
+    localStorage.setItem("contrast", "high");
+  }
+});
 
 const toggleButton = document.querySelector(".toggle");
 toggleButton.addEventListener("mousedown", () => {
@@ -91,3 +78,17 @@ toggleButton.addEventListener("mousedown", () => {
   html.style.setProperty("--primary-color", status ? `#212121` : `#901a36`);
   html.style.setProperty("--secondary-color", status ? `#545454` : `#749e2e`);
 });
+
+const toggleButton2 = document.querySelector(".toggle2");
+toggleButton2.addEventListener("mousedown", () => {
+  const status = toggleButton2.classList.toggle("off");
+  document.querySelector(".speech-status").textContent = status ? "til" : "fra";
+  localStorage.setItem("speech-status", status ? "speech-on" : " ");
+  location.reload();
+});
+if (localStorage.getItem("speech-status") === "speech-on") {
+  document.querySelector(".toggle2").classList.add("off");
+  document.querySelector(".speech-status").textContent = "til";
+} else {
+  document.querySelector(".speech-status").textContent = "fra";
+}
